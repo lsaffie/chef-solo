797fcf77d342a5243a85097e6a142b548544463a
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-08-17 15:40:13 -0400

    loading 1.9.2

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -16,22 +16,24 @@ bash "install rvm ruby 1.9.2" do
     echo "install ruby 1.9.2"
     rvm install 1.9.2 --with-openssl-dir=/usr/local/rvm/usr
 
-    echo "use 1.9.2"
-    rvm --default use 1.9.2
+    echo "rvm use 1.9.2" >> /etc/profile.d/rvm.sh
+    source /etc/profile.d/rvm.sh
 
-    echo "Add deploy to rvm group"
-    usermod -G rvm deploy
+    echo "use 1.9.2"
+    rvm use 1.9.2 --default
   EOH
 end
 
 bash "install bundler" do
   code <<-EOH
+    source /etc/profile.d/rvm.sh
     gem install bundler --no-rdoc --no-ri
   EOH
 end
 
 bash "install passenger" do
   code <<-EOH
+    source /etc/profile.d/rvm.sh
     gem install passenger --no-rdoc --no-ri
     /usr/local/rvm/gems/ruby-1.9.2-p290/bin/passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx
   EOH
