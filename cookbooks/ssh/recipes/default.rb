44c6f7c38f7194d0b80f29e7b2a1d80c13562305
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-08-17 16:09:10 -0400

    users

---
+++ cookbooks/ssh/recipes/default.rb
@@ -0,0 +1,8 @@
+user "deploy" do
+  comment "deploy user"
+  system true
+  shell "/bin/bash"
+  home "/home/deploy"
+  password "$1$9oRNaWmK$psmojmwlT10bmQjdXYvkH1"
+  supports :manage_home => true
+end
