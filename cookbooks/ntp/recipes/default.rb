57477c9ac487c833266ac660acec373848fb00b0
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-08-19 10:29:17 -0400

    fix line spacing error

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -2,8 +2,7 @@ package "ntp"
 
 bash "set ntp servers" do
   code <<-EOH
-    sed -i 's/server pool.ntp*/server 0.ca.pool.ntp.org\nserver 1.ca.pool.ntp.org\n
-    server 2.ca.pool.ntp.org\nserver 3.ca.pool.ntp.org/g'
+    sed -i 's/server pool.ntp*/server 0.ca.pool.ntp.org\nserver 1.ca.pool.ntp.org\n server 2.ca.pool.ntp.org\nserver 3.ca.pool.ntp.org/g'
   EOH
 end
 
