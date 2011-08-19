82f8b8d590b1988052cda5f74bb45e8e58eaf47b
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-08-19 11:00:25 -0400

    fixes

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -2,7 +2,7 @@ package "ntp"
 
 bash "set ntp servers" do
   code <<-EOH
-    sed -i 's/^server ntp.ubuntu.com/server 0.ca.pool.ntp.org\nserver 1.ca.pool.ntp.org\nserver 2.ca.pool.ntp.org\nserver 3.ca.pool.ntp.org/g' /etc/ntp.conf
+    sed -i 's/^server ntp.ubuntu.com/server 0.ca.pool.ntp.org\\nserver 1.ca.pool.ntp.org\\nserver 2.ca.pool.ntp.org\\nserver 3.ca.pool.ntp.org/g' /etc/ntp.conf
   EOH
 end
 
